80b57462408cd38cb55ae375e8293758190ad06f
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2019-07-21 21:09:46 +0900

    integrate TypeScript into this project

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,21 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <div>hello world!!</div>
+      <div>{{ message }}</div>
+      <v-btn @click="incr">!</v-btn>
     </v-flex>
   </v-layout>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class indexPage extends Vue {
+  message: string = 'hello world!!'
+
+  incr(): void {
+    this.message = this.message + '!'
+  }
+}
+</script>
