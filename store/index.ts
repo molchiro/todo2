f92e9c6c9cd949648737938cb1381864b7e98e21
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2019-07-23 00:46:28 +0900

    remove test module

--- store/index.ts
+++ store/index.ts
@@ -1,11 +1,9 @@
 import Vuex from 'vuex'
-import TestModule from '@/store/modules/test'
 import AuthModule from '@/store/modules/auth'
 
 const createStore = () => {
 	return new Vuex.Store({
 		modules: {
-      test: TestModule,
       auth: AuthModule
 		}
 	})
