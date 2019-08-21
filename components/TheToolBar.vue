86a7f293e21caba78f603a463653549e1f061ced
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2019-08-21 23:56:47 +0900

    use theToolBar component in titleOnly

--- components/TheToolBar.vue
+++ components/TheToolBar.vue
@@ -5,15 +5,20 @@
     fixed
     app
   )
-    v-toolbar-side-icon(@click.stop="toggleSideNav")
+    v-toolbar-side-icon(
+      v-if="showSideIcon"
+      @click.stop="toggleSideNav"
+    )
     v-toolbar-title Todo List
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Prop } from 'vue-property-decorator'
 
 @Component
 export default class ToolBar extends Vue {
+  @Prop({ default: true }) showSideIcon: boolean
+
   toggleSideNav(): void {
     this.$emit('toggleSideNav')
   }
--- layouts/titleOnly.vue
+++ layouts/titleOnly.vue
@@ -1,8 +1,19 @@
 <template lang="pug">
   v-app
-    v-toolbar(color="primary" dark fixed app)
-      v-toolbar-title Todo List
+    the-tool-bar(:showSideIcon="false")
     v-content
       v-container
         nuxt
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import TheToolBar from '@/components/TheToolBar.vue'
+
+@Component({
+  components: {
+    TheToolBar
+  }
+})
+export default class titleOnlyLayout extends Vue {}
+</script>
