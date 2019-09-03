ea67990ee93b98b1dc67867fbd823fcebcb76071
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2019-09-03 23:11:58 +0900

    migrate v-list

--- components/TheSideNav.vue
+++ components/TheSideNav.vue
@@ -6,9 +6,9 @@
     app
   )
     v-list
-      v-list-tile(@click="signOut")
-        v-list-tile-content
-          v-list-tile-title SIGNOUT
+      v-list-item(@click="signOut")
+        v-list-item-content
+          v-list-item-title SIGNOUT
 </template>
 
 <script lang="ts">
