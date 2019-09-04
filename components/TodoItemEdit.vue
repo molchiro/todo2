f5d646458b54ed9ba08eaa9ded17495e4ca6c9a8
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2019-09-04 21:25:53 +0900

    fix style

--- components/TodoItemEdit.vue
+++ components/TodoItemEdit.vue
@@ -1,20 +1,28 @@
 <template lang="pug">
   v-layout.py-2.grey.lighten-3
-    v-dialog(v-model="isShowDeleteDialog")
+    v-dialog(
+      v-model="isShowDeleteDialog"
+      max-width=300
+    )
       v-card
-        v-card-text 削除しますか？
+        v-card-title 削除しますか？
         v-divider
         v-card-actions
-          v-spacer
-          v-btn(
-            @click="isShowDeleteDialog = false"
-            text
-          ) いいえ
-          v-divider(vertical)
-          v-btn(
-            @click="deleteTodo"
-            text
-          ) はい
+          v-row(no-gutters)
+            v-col.pa-0
+              v-btn(
+                @click="isShowDeleteDialog = false"
+                text
+                block
+              ) キャンセル
+            v-divider(vertical)
+            v-col.pa-0
+              v-btn(
+                @click="deleteTodo"
+                text
+                block
+                color="red"
+              ) 削除
     v-row(no-gutters)
       v-col(offset=1)
         v-text-field.white(
