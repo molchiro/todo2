313bebafa510220a6d4529253a5505ae8a832e22
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2019-09-25 23:21:22 +0900

    restore TodoPost Component

---
+++ components/TodoPost.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+  v-card
+    v-container.px-2.py-0
+      v-row(no-gutters)
+        v-col.pr-1(offset=1)
+          v-text-field(
+            v-model="todo.content"
+            @keypress.enter="addTodo()"
+          )
+        v-col.text-center.align-self-center(cols=1)
+          v-icon(
+            @click="addTodo()"
+            :color="todo.isValid() ? 'primary' : 'grey'"
+          ) create
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from 'nuxt-property-decorator'
+import { getModule } from 'vuex-module-decorators'
+import TodosModule from '@/store/modules/todos'
+import AuthModule from '@/store/modules/auth'
+import { Todo } from '@/models/todo'
+
+@Component
+export default class TodoPost extends Vue {
+  todosModule = getModule(TodosModule, this.$store)
+
+  authModule = getModule(AuthModule, this.$store)
+
+  @Prop() readonly projectId: string
+
+  todo = new Todo({
+    uid: this.authModule.currentUserUid,
+    projectId: this.projectId
+  })
+
+  @Watch('todosModule.maxPriority')
+  onMaxPriorityChanged(newVal: number) {
+    this.todo.priority = newVal + 1
+  }
+
+  addTodo(): void {
+    if (this.todo.isValid()) {
+      this.todosModule.addTodo(this.todo)
+      this.todo = new Todo({
+        uid: this.authModule.currentUserUid,
+        projectId: this.projectId
+      })
+    }
+  }
+}
+</script>
