2fdcda498c48f40beb6cd11723d2aacabcd8055d
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2019-10-09 00:19:40 +0900

    fix style

--- components/ProjectListItem.vue
+++ components/ProjectListItem.vue
@@ -1,15 +1,9 @@
 <template lang="pug">
   v-list-item
-    v-row(
-      dense
-      align="center"
-    )
-      v-col(cols=1)
-        v-list-item-icon
-          v-icon
-            slot(name="icon")
-      v-col.pl-3
-        v-list-item-content
-          v-list-item-title
-            slot(name="title")
+    v-list-item-icon.mr-1
+      v-icon
+        slot(name="icon")
+    v-list-item-content
+      v-list-item-title
+        slot(name="title")
 </template>
