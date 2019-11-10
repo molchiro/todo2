69ef7b7b8549ade197b4d6cd151806a424b64396
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2019-11-11 00:56:21 +0900

    readable

--- components/TodoListItemShow.vue
+++ components/TodoListItemShow.vue
@@ -1,19 +1,17 @@
 <template lang="pug">
   v-container.pa-0.my-3
-    v-row.pl-8.cursor-copy(
-      v-if="isNew"
-      @click="$emit('startEdit')"
-    )
-      v-icon.grey--text.ml-n6 add
-      v-col.py-0
-        div.grey--text 新しいTODOを追加
-    v-row.pl-8(v-else)
-      v-checkbox.justify-center.ma-0.ml-n6.pa-0(
-          v-model="done"
-          hide-details
-        )
-      v-col.py-0.pl-1.cursor-text
-        div(@click="$emit('startEdit')") {{ todo.content }}
+    v-row.pl-8
+      template(v-if="isNew")
+        v-icon.grey--text.ml-n6(@click="$emit('startEdit')") add
+        v-col.py-0.cursor-pointer(@click="$emit('startEdit')")
+          div.grey--text 新しいTODOを追加
+      template(v-else)
+        v-checkbox.justify-center.ma-0.ml-n6.pa-0(
+            v-model="done"
+            hide-details
+          )
+        v-col.py-0.pl-1.cursor-text
+          div(@click="$emit('startEdit')") {{ todo.content }}
 </template>
 
 <script lang="ts">
