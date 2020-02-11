3f601200f42d520eaffe2852744dbc1e48bedfac
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2020-02-11 17:56:03 +0900

    share dialog

--- pages/projects/_id.vue
+++ pages/projects/_id.vue
@@ -6,6 +6,35 @@
       :onClickCancel="closeDeleteDialog"
     ) このプロジェクトに紐づくTODOも全て削除されます。
       | この操作は取り消せません。
+    v-dialog(
+      v-model="isOpenShareDialog"
+      max-width=300
+    )
+      v-card
+        v-card-title 共有の設定
+        div(v-if="localProject.canInvite")
+          v-card-text 招待したい人に以下のURLを共有してください
+          v-text-field(
+            v-model="invitationUrl"
+            readonly
+          )
+          v-btn(@click="disableInvitation()") 招待を中止
+        div(v-else)
+          v-card-text このプロジェクトは共有されていません
+          v-btn(@click="enableInvitation()") 招待URLを取得
+        v-divider
+        v-card-actions
+          v-row(no-gutters)
+            v-col.pa-0
+              v-btn(
+                @click="isOpenShareDialog = false"
+                text
+                block
+              ) 閉じる
+    v-snackbar(
+      v-model="isShowSnackbar"
+      bottom
+    ) {{ snackbarText}}
     v-container.px-2
       v-row(no-gutters)
         v-col(cols=10)
@@ -17,8 +46,7 @@
             )
         v-col.align-self-center.text-center(cols=1)
           v-icon(
-            @click="enableInvitation()"
-            :color="localProject.canInvite ? 'primary' : 'gray'"
+            @click="isOpenShareDialog = true"
           ) share
         v-col.align-self-center.text-center(cols=1)
           v-icon(@click="openDeleteDialog()") delete
@@ -52,6 +80,12 @@ const projectsRef = db.collection('projects')
 export default class projectPage extends Vue {
   isOpenDeleteDialog: boolean = false
 
+  isOpenShareDialog: boolean = false
+
+  isShowSnackbar: boolean = false
+
+  snackbarText: string = ''
+
   get projectId(): string {
     const id: string = this.$route.params.id
     projectsStore.setSelectedProjectId(id)
@@ -79,6 +113,14 @@ export default class projectPage extends Vue {
     return isChanged && this.localProject.isValid()
   }
 
+  get host(): string {
+    return location.host
+  }
+
+  get invitationUrl(): string {
+    return `https://${this.host}/invitation?code=${this.localProject.invitationCode}`
+  }
+
   created(): void {
     todosStore.bindTodos(this.projectId)
   }
@@ -101,15 +143,21 @@ export default class projectPage extends Vue {
   }
 
   enableInvitation(): void {
-    this.localProject.canInvite = true
-    this.localProject.invitationCode = generateUuid4()
-    projectsStore.updateProject(this.localProject)
+    if (!this.localProject.canInvite) {
+      this.localProject.canInvite = true
+      this.localProject.invitationCode = generateUuid4()
+      projectsStore.updateProject(this.localProject)
+      this.snackbarText = '招待URLをコピーしました'
+      this.isShowSnackbar = true
+    }
   }
 
   disableInvitation(): void {
-    this.localProject.canInvite = false
-    this.localProject.invitationCode = ''
-    projectsStore.updateProject(this.localProject)
+    if (this.localProject.canInvite) {
+      this.localProject.canInvite = false
+      this.localProject.invitationCode = ''
+      projectsStore.updateProject(this.localProject)
+    }
   }
 
   async deleteProject(): Promise<void> {
